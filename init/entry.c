5dd40c6c96cdfd0aada1259f8a2a1a13594d3606
Sicheng Chen <csc0330@hotmail.com>
2021-11-18 09:17:18 +0800

    Update entry.c

--- init/entry.c
+++ init/entry.c
@@ -11,17 +11,6 @@ kern_entry()
     *input++ = 'l'; *input++ = color;
     *input++ = 'l'; *input++ = color;
     *input++ = 'o'; *input++ = color;
-    *input++ = ','; *input++ = color;
-    *input++ = ' '; *input++ = color;
-    *input++ = 'O'; *input++ = color;
-    *input++ = 'S'; *input++ = color;
-    *input++ = ' '; *input++ = color;
-    *input++ = 'K'; *input++ = color;
-    *input++ = 'e'; *input++ = color;
-    *input++ = 'r'; *input++ = color;
-    *input++ = 'n'; *input++ = color;
-    *input++ = 'e'; *input++ = color;
-    *input++ = 'l'; *input++ = color;
     *input++ = '!'; *input++ = color;
 
     return 0;
