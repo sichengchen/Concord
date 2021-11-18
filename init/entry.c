6c9925b0169881e64089d6dbb0a6a63096934709
Sicheng Chen <csc0330@hotmail.com>
2021-11-18 09:16:55 +0800

    Update entry.c

--- init/entry.c
+++ init/entry.c
@@ -3,9 +3,26 @@
 int
 kern_entry()
 {
-  unsigned char *input = (unsigned char*)0xB8000;
-  unsigned char color = (0<<4) | (15&0x0F);
-  *input++ = 'T';
-  *input++ = color;
-  return 0;
+    uint8_t *input = (uint8_t *)0xB8000;
+    uint8_t color = (0 << 4) | (15 & 0x0F);
+
+    *input++ = 'H'; *input++ = color;
+    *input++ = 'e'; *input++ = color;
+    *input++ = 'l'; *input++ = color;
+    *input++ = 'l'; *input++ = color;
+    *input++ = 'o'; *input++ = color;
+    *input++ = ','; *input++ = color;
+    *input++ = ' '; *input++ = color;
+    *input++ = 'O'; *input++ = color;
+    *input++ = 'S'; *input++ = color;
+    *input++ = ' '; *input++ = color;
+    *input++ = 'K'; *input++ = color;
+    *input++ = 'e'; *input++ = color;
+    *input++ = 'r'; *input++ = color;
+    *input++ = 'n'; *input++ = color;
+    *input++ = 'e'; *input++ = color;
+    *input++ = 'l'; *input++ = color;
+    *input++ = '!'; *input++ = color;
+
+    return 0;
 }
