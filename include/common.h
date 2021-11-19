5cce9974b196701b07061b112e882d51f6ed251d
Sicheng Chen <csc0330@hotmail.com>
2021-11-19 16:40:02 +0800

    Update common.h

--- include/common.h
+++ include/common.h
@@ -1,15 +1,16 @@
+
 #ifndef INCLUDE_COMMON_H_
 #define INCLUDE_COMMON_H_
 
 #include "types.h"
 
-void
+void 
 outb(uint16_t port, uint8_t value);
 
-uint8_t
+uint8_t 
 inb(uint16_t port);
 
-uint16_t
+uint16_t 
 inw(uint16_t port);
 
-#endif //INCLUDE_COMMON_H
+#endif // INCLUDE_COMMON_H_
