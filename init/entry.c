b01a960dd08551c88e2d6e95def9dd49d83533f8
Sicheng Chen <csc0330@hotmail.com>
2021-11-19 19:29:18 +0800

    Update entry.c

--- init/entry.c
+++ init/entry.c
@@ -1,10 +1,9 @@
-#include "types.h"
 #include "console.h"
 
 int
 kern_entry()
 {
-  console_clear();
-  console_write_color("Concord Kernel is starting...\n", rc_black, rc_green);
-  return 0;
+	console_clear();
+	console_write_color("Hello!\n", rc_black, rc_green);
+	return 0;
 }
