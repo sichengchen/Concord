c9f82aebece5ef51fe293e7f39a2a0b77d4045a9
Sicheng Chen <csc0330@hotmail.com>
2021-11-21 09:45:34 +0800

    finished string

--- include/string.h
+++ include/string.h
@@ -3,30 +3,30 @@
   #include "types.h"
 
 void*
-memset(void* dst, int c, uint32_t n);
+memset(void *dst, int c, uint32_t n);
 
 int
-memcmp(const void* v1, const void* v2,
+memcmp(const void *v1, const void *v2,
        uint32_t n);
 
 void*
-memmove(void* dst, const void* src,
+memmove(void *dst, const void *src,
         uint32_t n);
 
 void*
-memcpy(void* dst, const void* src, uint32_t n);
+memcpy(void *dst, const void *src, uint32_t n);
 
 int
-strncmp(const char* p, const char* q,
+strncmp(const char *p, const char *q,
         uint32_t n);
 
 char*
-strncpy(char* s, const char* t, uint32_t n);
+strncpy(char *s, const char *t, uint32_t n);
 
 char*
-safestrcpy(char* s, const char* t, uint32_t n);
+safestrcpy(char *s, const char *t, uint32_t n);
 
 int
-strlen(const char* s);
+strlen(const char *s);
 
 #endif
--- libs/string.c
+++ libs/string.c
@@ -1,9 +1,9 @@
 #include "string.h"
 
 void*
-memset(void* dst, int c, uint32_t n)
+memset(void *dst, int c, uint32_t n)
 {
-  char* cdst = (char*) dst;
+  char *cdst = (char*) dst;
   int i;
   for(i = 0; i < n; i++){
     cdst[i] = c;
@@ -12,7 +12,7 @@ memset(void* dst, int c, uint32_t n)
 }
 
 int
-memcmp(const void* v1, const void* v2,
+memcmp(const void *v1, const void *v2,
        uint32_t n)
 {
   const unsigned char *s1, *s2;
@@ -29,11 +29,11 @@ memcmp(const void* v1, const void* v2,
 }
 
 void*
-memmove(void* dst, const void* src,
+memmove(void *dst, const void *src,
         uint32_t n)
 {
-  const char* s = src;
-  char* d = dst;
+  const char *s = src;
+  char *d = dst;
   if(s < d && s + n > d){
     s += n;
     d += n;
@@ -49,8 +49,53 @@ memmove(void* dst, const void* src,
 }
 
 void*
-memcpy(void* dst, const void* src, uint32_t n)
+memcpy(void *dst, const void *src, uint32_t n)
 {
   return memmove(dst, src, n);
 }
 
+int
+strncmp(const char *p, const char *q,
+        uint32_t n)
+{
+  while(n > 0 && *p && *p == *q){
+    n--;
+    p++;
+    q++;
+  }
+  if(n == 0){
+    return 0;
+  }
+  return (unsigned char)*p - (unsigned char)*q;
+}
+
+char*
+strncpy(char *s, const char *t, uint32_t n)
+{
+  char *os;
+  os = s;
+  while(n-- > 0 && (*s++ = *t++) != 0);
+  *s = 0;
+  return os;
+}
+
+char*
+safestrcpy(char *s, const char *t, uint32_t n)
+{
+  char *os;
+  os = s;
+  if(n <= 0){
+    return os;
+  }
+  while(--n > 0 && (*s++ = *t++) != 0);
+  *s = 0;
+  return os;
+}
+
+int
+strlen(const char *s)
+{
+  int n;
+  for(n = 0; s[n]; n++);
+  return n;
+}
